dfe8fb8dfdcdaf51bd93aca14911d3257f51dbbe
PengJunhuan <13051638633@163.com>
2017-11-30 16:52:31 +0800

    添加了源代码约定、增加了FileReader类、添加了用于测试的demo文件

---
+++ src/FileReader.py
@@ -0,0 +1,51 @@
+class FileReader:
+    """
+    文件读取类——用于从指定的文件中读取源码
+    """
+
+    __result = None
+
+    def __init__(self, filename=None):
+        """
+
+        :param filename: 文件名
+        :raises: FileNotFindError
+        """
+        if filename is None:
+            return
+        elif type(filename) is not str:
+            raise TypeError('文件名要求为字符串！')
+        else:
+            try:
+                with open(filename, 'r') as f:
+                    self.__result = "".join(f.readlines())
+            except FileNotFoundError as e:
+                print(e)
+
+    def getresult(self):
+        """
+        返回源码文本，如果未加载文件或未正确加载文件则返回一个空列表
+
+        :rtype: list
+        """
+        if self.__result is None:
+            return []
+        else:
+            return self.__result
+
+    def fileload(self, filename):
+        """
+        加载指定文件
+        :param filename:
+        :raise: FileNotFoundError
+        """
+        try:
+            with open(filename,'r') as f:
+                self.__result = "".join(f.readlines())
+        except FileNotFoundError as e:
+            print(e)
+
+
+if __name__ == '__main__':
+    fr = FileReader('demo.txt')
+    print(fr.getresult())
